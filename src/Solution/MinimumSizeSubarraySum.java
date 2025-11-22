40410d62f77370244e6e49990051f484b8df59f9
Ryan Jukyeong Kim <ryan.jukyeong.kim@gmail.com>
2025-11-22 22:17:17 +0800

    Commit - JKKIM 11/22/2025

---
+++ src/Solution/MinimumSizeSubarraySum.java
@@ -0,0 +1,18 @@
+package Solution;
+
+public class MinimumSizeSubarraySum {
+    //209. Minimum Size Subarray Sum - https://leetcode.com/problems/minimum-size-subarray-sum/description/
+    public static int minSubArrayLen(int target, int[] nums) {
+        int left = 0, sum = 0, minCount = Integer.MAX_VALUE;
+
+        for(int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target){
+                minCount = Math.min(minCount, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+
+        return minCount == Integer.MAX_VALUE ? 0 : minCount;
+    }
+}
