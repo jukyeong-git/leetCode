5b19cbac7984317a6ac9db3ff484e6ffc4057561
Ryan Jukyeong Kim <ryan.jukyeong.kim@gmail.com>
2026-01-28 17:15:16 +0800

    Commit - JKKIM 28/01/2026

--- src/Solution/Ransom_Note.java
+++ src/Solution/Ransom_Note.java
@@ -4,6 +4,27 @@ import java.util.*;
 
 public class Ransom_Note {
     //383. Ransom Note - https://leetcode.com/problems/ransom-note/description/
+
+    /*
+     * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+     * Each letter in magazine can only be used once in ransomNote.
+     *
+     * Example 1:
+     *      Input: ransomNote = "a", magazine = "b"
+     *      Output: false
+     *
+     * Example 2:
+     *      Input: ransomNote = "aa", magazine = "ab"
+     *      Output: false
+     *
+     * Example 3:
+     *      Input: ransomNote = "aa", magazine = "aab"
+     *      Output: true
+     *
+     * Constraints:
+     *      1 <= ransomNote.length, magazine.length <= 105
+     *      ransomNote and magazine consist of lowercase English letters.
+     */
     public static boolean canConstruct(String ransomNote, String magazine) {
 
         Map<Character, Integer> mMap = new HashMap<>();
--- src/Solution/Remove_Duplicates_from_Sorted_List_II.java
+++ src/Solution/Remove_Duplicates_from_Sorted_List_II.java
@@ -10,16 +10,16 @@ public class Remove_Duplicates_from_Sorted_List_II {
      * Return the linked list sorted as well.
      *
      * Example 1:
-     *  Input: head = [1,2,3,3,4,4,5]
-     *  Output: [1,2,5]
+     *      Input: head = [1,2,3,3,4,4,5]
+     *      Output: [1,2,5]
      *
      * Example 2:
-     *  Input: head = [1,1,1,2,3]
-     *  Output: [2,3]
+     *      Input: head = [1,1,1,2,3]
+     *      Output: [2,3]
      *
      * Constraints:
-     *  The number of nodes in the list is in the range [0, 300].
-     *  -100 <= Node.val <= 100
+     *      The number of nodes in the list is in the range [0, 300].
+     *      -100 <= Node.val <= 100
      *
      * The list is guaranteed to be sorted in ascending order.
      */
